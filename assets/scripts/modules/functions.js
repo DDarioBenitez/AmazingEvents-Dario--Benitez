0afe305908087c5bf319983f6eba4e7441b1a298
DDarioBenitez <reedq2.alt@gmail.com>
2023-07-21 17:07:09 -0300

    changes

--- assets/scripts/modules/functions.js
+++ assets/scripts/modules/functions.js
@@ -1,5 +1,5 @@
 //Cards
-export function newCard(data){
+export function newCard(data) {
     return `<article class="main-card card col-10 p-2 col-lg-5">
     <img src="${data.image}" class="card-img-top" alt="">
     <div class="card-body d-xl-flex flex-xl-column justify-content-xl-between">
@@ -12,7 +12,7 @@ export function newCard(data){
     </div>
 </article>`
 }
-export function newCardPastAndFuture(data){
+export function newCardPastAndFuture(data) {
     return `<article class="main-card card col-10 p-2 col-lg-5">
     <img src="${data.image}" class="card-img-top" alt="foot-fair.img">
     <div class="card-body d-xl-flex flex-xl-column justify-content-xl-between">
@@ -26,9 +26,9 @@ export function newCardPastAndFuture(data){
 </article>`
 }
 
-export function imprCard(data,elementHtml,newCard){
-    let template=""
-    for (let infNewCard of data){
+export function imprCard(data, elementHtml, newCard) {
+    let template = ""
+    for (let infNewCard of data) {
         template += newCard(infNewCard)
     }
     elementHtml.innerHTML += template
@@ -36,55 +36,55 @@ export function imprCard(data,elementHtml,newCard){
 
 //Create Checkbox
 
-export function newCheck(data){
+export function newCheck(data) {
     return `<div class=" main-check form-check pt-1 p-1 m-0 text-start col-md-3 fs-4 col-xl-2 col-xxl-1">
-        <input class="form-check-input" type="checkbox" value= "${data}" id="flexCheckDefault1">
-        <label class="form-check-label" for="flexCheckDefault1">
+        <input class="form-check-input" type="checkbox" value= "${data}" id="${data}">
+        <label class="form-check-label" for="${data}">
             ${data}
         </label>
     </div>`
- }
+}
 
-export function imprCheck(data, elementHtml,newCheck){
-    let template=""
+export function imprCheck(data, elementHtml, newCheck) {
+    let template = ""
     for (let check of data) {
         template += newCheck(check)
     }
     elementHtml.innerHTML += template
- }
+}
 
 //Clear elementHTML
-export function empty(elementHtml){
-    elementHtml.innerHTML=""
+export function empty(elementHtml) {
+    elementHtml.innerHTML = ""
 }
 
 //Error mesage
-export function errorMesage(elementHtml){
-    return elementHtml.innerHTML ="<h5>No matches found</h5>"
+export function errorMesage(elementHtml) {
+    return elementHtml.innerHTML = "<h5>No matches found</h5>"
 }
 
 //Filters 
-export function arrayChecks(arrayChecks){
-    let aux=arrayChecks.filter(check=> check.checked == true)
-    return aux.map(check=> check.value)
+export function arrayChecks(arrayChecks) {
+    let aux = arrayChecks.filter(check => check.checked == true)
+    return aux.map(check => check.value)
 }
 
-export function includesCardsOfSearch(events,input){
-    let filter =  events.filter(event => event.name.toLowerCase().includes(input.value.toLowerCase()) )
+export function includesCardsOfSearch(events, input) {
+    let filter = events.filter(event => event.name.toLowerCase().includes(input.value.toLowerCase()))
     return filter
-  }
+}
 
-export function filterCardsCheck(events,checkValue){
-    if(checkValue.length==0){
+export function filterCardsCheck(events, checkValue) {
+    if (checkValue.length == 0) {
         return events
-    }else{
-        let filter =  events.filter(event => checkValue.includes(event.category) )
+    } else {
+        let filter = events.filter(event => checkValue.includes(event.category))
         return filter
     }
-  }
+}
 
-  // filtro cruzado
-export function filterConvined(events,checkValue,searchValue){
-    let check = filterCardsCheck(includesCardsOfSearch(events,searchValue),checkValue)
-   return check
-  }
+// filtro cruzado
+export function filterConvined(events, checkValue, searchValue) {
+    let check = filterCardsCheck(includesCardsOfSearch(events, searchValue), checkValue)
+    return check
+}
--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -1,6 +1,6 @@
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
     .then(data => data.json())
-    .then(data=>{
+    .then(data => {
         //Contenedores
         const tableFuture = document.getElementById("table-future")
         const tablePast = document.getElementById("table-past")
@@ -14,108 +14,108 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")
         console.log(eventsPast);
         let events = data.events
         console.log((events));
-        let currentDate= data.currentDate
-        let categorysPast= Array.from(new Set(eventsPast.map(event=> event.category)))
-        let categorysFutures= Array.from(new Set(eventsFutures.map(event=> event.category)))
-        let futures = filtrarByCategory(eventsFutures,categorysFutures)
-        let past = filtrarByCategory(eventsPast,categorysPast)
+        let currentDate = data.currentDate
+        let categorysPast = Array.from(new Set(eventsPast.map(event => event.category)))
+        let categorysFutures = Array.from(new Set(eventsFutures.map(event => event.category)))
+        let futures = filtrarByCategory(eventsFutures, categorysFutures)
+        let past = filtrarByCategory(eventsPast, categorysPast)
         //Ejecuciones
-        firstTabla(eventsPast,events, firstTable)
-        
-        imprEventLine(futures,tableFuture,createEventLine)
-        imprEventLine(past,tablePast,createEventLine)
-    })
+        firstTabla(eventsPast, events, firstTable)
+
+        imprEventLine(futures, tableFuture, createEventLine)
+        imprEventLine(past, tablePast, createEventLine)
+    }).catch(error => console.log(("fallo")))
 
 // 
 // //Funciones
 
-function firstTabla(eventPast,events, elementoHtml){
- return elementoHtml.innerHTML = `<tr class="text-center align-middle" id="">
+function firstTabla(eventPast, events, elementoHtml) {
+    return elementoHtml.innerHTML = `<tr class="text-center align-middle" id="">
  <td class="text-danger bg-light-subtle fs-5 align-middle">${mayorPorcentajeAssitance(eventPast)}</td>
  <td class="text-danger bg-light-subtle fs-5 align-middle">${minumPorcentajeAssistance(eventPast)}</td>
  <td class="text-danger bg-light-subtle fs-5 align-middle">${mayorCapacity(events)}</td>
 </tr>`
 }
-function createEventLine(event){
+function createEventLine(event) {
     return `<tr class="text-center align-middle">
     <td class="text-danger bg-light-subtle fs-5 align-middle">${event.category}</td>
     <td class="text-danger-emphasis bg-light-subtle align-middle">$${event.revenue.toLocaleString()} USD</td>
-    <td class="text-danger-emphasis bg-light-subtle align-middle">% ${event.porcentageOfAssitance}</td>
+    <td class="text-danger-emphasis bg-light-subtle align-middle">${event.porcentageOfAssitance} %</td>
 </tr>`
 }
 
-function imprEventLine(array, elementoHtml,newLine){
-    let template=""
-    for(let event of array){
+function imprEventLine(array, elementoHtml, newLine) {
+    let template = ""
+    for (let event of array) {
         template += newLine(event)
     }
     return elementoHtml.innerHTML += template
 }
-function revenues(event, currentDate){
-    return currentDate<event.date ? event.price * event.estimate : event.price * event.assistance
+function revenues(event, currentDate) {
+    return currentDate < event.date ? event.price * event.estimate : event.price * event.assistance
 }
-function porcentageAssitance(event, currentDate){
-    return currentDate<event.date ? Math.round(100*event.estimate/event.capacity) : Math.round(100*event.assistance/event.capacity)
+function porcentageAssitance(event, currentDate) {
+    return currentDate < event.date ? Math.round(100 * event.estimate / event.capacity) : Math.round(100 * event.assistance / event.capacity)
 }
-function filtrarByCategory(events, categorys){
-    let resultadoMap = categorys.map(category=>{
-        let eventosDeLaCategoria= events.filter(event=>event.category==category)
-        let capacityTotal=0
-        let assistanceTotal=0
-        let objetoFinal={
+function filtrarByCategory(events, categorys) {
+    let resultadoMap = categorys.map(category => {
+        let eventosDeLaCategoria = events.filter(event => event.category == category)
+        let capacityTotal = 0
+        let assistanceTotal = 0
+        let objetoFinal = {
             category: category,
             revenue: 0,
-            porcentageOfAssitance:0,
+            porcentageOfAssitance: 0,
         }
-        for(let event of eventosDeLaCategoria){
-           objetoFinal.revenue += event.price * (event.assistance || event.estimate)
-           capacityTotal += event.capacity
-           assistanceTotal += (event.assistance || event.estimate)
-           objetoFinal.porcentageOfAssitance= Math.round(100*assistanceTotal/capacityTotal)
-        } 
+        for (let event of eventosDeLaCategoria) {
+            objetoFinal.revenue += event.price * (event.assistance || event.estimate)
+            capacityTotal += event.capacity
+            assistanceTotal += (event.assistance || event.estimate)
+            objetoFinal.porcentageOfAssitance = Math.round(100 * assistanceTotal / capacityTotal)
+        }
         return objetoFinal
-    } )
-    return resultadoMap   
+    })
+    return resultadoMap
 }
 
-function mayorPorcentajeAssitance(events){
-    let control=0
+function mayorPorcentajeAssitance(events) {
+    let control = 0
     let aux
     let eventSelec
-    for(let event of events){
-        aux = Math.round(100*event.assistance/event.capacity)
-        if (control<=aux){
-            control=aux
-            eventSelec=event.name
+    for (let event of events) {
+        aux = Math.round(100 * event.assistance / event.capacity)
+        if (control <= aux) {
+            control = aux
+            eventSelec = event.name
         }
     }
-    return eventSelec
+    return eventSelec = `"${eventSelec}",  ${aux} %`
 }
 
-function minumPorcentajeAssistance(events){
-    let control = Math.round(100*events[0].assistance/events[0].capacity)
+function minumPorcentajeAssistance(events) {
+    let control = Math.round(100 * events[0].assistance / events[0].capacity)
     let aux
     let eventSelec
-    for(let event of events){
-        aux = Math.round(100*event.assistance/event.capacity)
-        if (control>=aux){
-            control=aux
-            eventSelec=event.name
+    for (let event of events) {
+        aux = Math.round(100 * event.assistance / event.capacity)
+        if (control >= aux) {
+            control = aux
+            eventSelec = event.name
         }
     }
-    return eventSelec
+    return eventSelec = `"${eventSelec}",  ${aux} %`
 }
 
-function mayorCapacity(events){
-    let control=0
+function mayorCapacity(events) {
+    let control = 0
     let aux
     let eventSelec
-    for(let event of events){
+    for (let event of events) {
         aux = event.capacity
-        if (control<=aux){
-            control=aux
-            eventSelec=event.name
+        if (control <= aux) {
+            control = aux
+            eventSelec = event.name
         }
     }
-    return eventSelec
+    return `"${eventSelec}"`
 }
